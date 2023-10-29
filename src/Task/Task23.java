093a3b89b3cc824fe6effa0cbeb0085914ae3e70
Foat <Khisamov.foat@mail.ru>
2023-10-29 20:21:11 +1000

    Задачки 29.10.2023

--- src/Task/Task23.java
+++ src/Task/Task23.java
@@ -1,6 +1,5 @@
 package Task;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 // 1. Напишите программу, которая принимает на вход число (N)
@@ -9,6 +8,15 @@ import java.util.Scanner;
 // 5 -> 1, 8, 27, 64, 125
 public class Task23 {
     public static void main(String[] args) {
-
+        System.out.print("Ведите число N: ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        int count = 1;
+        System.out.printf("%d -> ", number);
+        for (int i = 1; i < number; i++) {
+            count = i * i * i;
+            System.out.printf("%d, ", count);
+        }
+        System.out.printf("%d",number * number * number);
     }
 }
