4c332c469614245cca600cb989e21539eeb03f20
Foat <Khisamov.foat@mail.ru>
2023-11-08 19:11:13 +1000

    Задачки 08.11.2023

---
+++ src/OOP/Lesson6/Work/Work00_19.java
@@ -0,0 +1,10 @@
+package OOP.Lesson6.Work;
+
+import java.util.Arrays;
+
+public class Work00_19 {
+    public static void main(String[] args) {
+        Work00 result = new Work00();
+        System.out.println(Arrays.toString(result.array(8)));
+    }
+}
---
+++ src/OOP/Lesson6/Work/Work00_21.java
@@ -0,0 +1,10 @@
+package OOP.Lesson6.Work;
+
+import java.util.Arrays;
+
+public class Work00_21 {
+    public static void main(String[] args) {
+        Work00 result = new Work00();
+        System.out.println(Arrays.toString(result.sumNumbers(12)));
+    }
+}
