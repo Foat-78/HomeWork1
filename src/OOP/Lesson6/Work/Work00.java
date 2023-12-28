3388e67c4f4076c82aeb7a9f9acda1a5e2901499
Foat <Khisamov.foat@mail.ru>
2023-12-28 11:40:32 +1000

    Самостоятельная работа 28.12.2023

--- src/OOP/Lesson6/Work/Work00.java
+++ src/OOP/Lesson6/Work/Work00.java
@@ -618,4 +618,24 @@ public class Work00 {
         return String.format("%s -> %s", num1, counter);
     }
 
+    //Задача 1 с Leetcode
+    //Учитывая массив целых чисел nums и целое число target, верните индексы двух чисел так, чтобы их сумма составляла target .
+    //Вы можете предположить, что каждый вход будет иметь ровно одно решение , и вы не можете использовать один и тот же элемент дважды.
+    //Вы можете вернуть ответ в любом порядке.
+    //Пример 1:
+    //Ввод: nums = [2,7,11,15], target = 9
+    //Выход: [0,1]
+    //Объяснение: поскольку nums[0] + nums[1] == 9, мы возвращаем [0, 1].
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    res = new int[]{i, j};
+                }
+            }
+        }
+        return res;
+    }
 }
---
+++ src/OOP/Lesson6/Work/Work00_33.java
@@ -0,0 +1,11 @@
+package OOP.Lesson6.Work;
+
+import java.util.Arrays;
+
+public class Work00_33 {
+    public static void main(String[] args) {
+        Work00 result = new Work00();
+        int[] num = {3, 3, 4, 5};
+        System.out.println(Arrays.toString(result.twoSum(num,6)));
+    }
+}
